b3a968dd29a52ab2f2d0293782f1a3a1be87e93a
Nikhil Garg <nikhil.garg@prudential.com.sg>
2022-01-10 16:49:33 +0530

    Used custom hook for sending request

--- src/App.js
+++ src/App.js
@@ -3,41 +3,31 @@ import React, { useEffect, useState } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
+import useHttp from './hooks/use-http';
+
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-9242e-default-rtdb.firebaseio.com/tasks.json'
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
 
-      const data = await response.json();
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
+
+
+  useEffect(() => {
+    const transformTask = (tasksObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    };
+    fetchTasks({
+      url: 'https://react-http-9242e-default-rtdb.firebaseio.com/tasks.json',
+    }, transformTask);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const useHttp = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const sendRequest = async (requestConfig, applyData) => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
         setIsLoading(true);
         setError(null);
         try {
             const response = await fetch(
                 requestConfig.url, {
-                method: requestConfig.method,
-                headers: requestConfig.headers,
-                body: JSON.stringify(requestConfig.body)
+                method: requestConfig.method ? requestConfig.method : "GET",
+                headers: requestConfig.headers ? requestConfig.headers : {},
+                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
             }
             );
 
@@ -28,7 +28,7 @@ const useHttp = () => {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    }, []);
 
     return {
         isLoading,
